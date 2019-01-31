b634e3f49c3221c53d91a910f8247a3af69a67c2
Diana Badas <diana.badas@gmail.com>
2019-01-31 09:55:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,16 +2,12 @@
 require 'nokogiri'
 require 'pry'
 
-class Scraper
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
 
-  def create_project_hash
-    html = File.read('fixtures/kickstarter.html')
-
-    kickstarter = Nokogiri::HTML(html)
-  end
-
+  kickstarter = Nokogiri::HTML(html)
+    
   binding.pry
-
 end
 
 create_project_hash
